2c6182bdf92d87cb73706527fb9ef41fd1b8b310
Matthew Normyle <mmnormyle@gmail.com>
2016-11-18 23:56:11 -0600

    initial commit

--- LatexDemo/MainWindow.xaml.cs
+++ LatexDemo/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LatexDemo
 {
@@ -23,6 +17,53 @@ namespace LatexDemo
         public MainWindow()
         {
             InitializeComponent();
+            btnPredict.Click += btnPredictClick;
+        }
+
+        private void runScript()
+        {
+            string cmd = "C:\\Users\\mmnor\\Projects\\autolatex\\predict.py";
+            string imageFileName = "C:/Users/mmnor/Projects/autolatex/poopy.png";
+            string clfLocation = "C:/Users/mmnor/Projects/autolatex/MLP.p";
+            string pythonName = "C:\\Anaconda3\\python.exe";
+            Process process = new Process();
+            process.StartInfo.FileName = pythonName;
+            process.StartInfo.Arguments = cmd + " " + imageFileName + " " + clfLocation;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            textBox.Text = output;
+        }
+
+        private void saveCanvas(string fullFileName)
+        {
+            Rect bounds = VisualTreeHelper.GetDescendantBounds(ink);
+            double dpi = 96d;
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)bounds.Width, (int)bounds.Height, dpi, dpi, System.Windows.Media.PixelFormats.Default);
+            DrawingVisual dv = new DrawingVisual();
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                VisualBrush vb = new VisualBrush(ink);
+                dc.DrawRectangle(vb, null, new Rect(new Point(), bounds.Size));
+            }
+            rtb.Render(dv);
+            BitmapEncoder pngEncoder = new PngBitmapEncoder();
+            pngEncoder.Frames.Add(BitmapFrame.Create(rtb));
+            using (MemoryStream ms = new System.IO.MemoryStream())
+            {
+                pngEncoder.Save(ms);
+                File.WriteAllBytes(fullFileName, ms.ToArray());
+            }
+        }
+
+        private void btnPredictClick(object sender, RoutedEventArgs e)
+        {
+            string fileName = "C:\\Users\\mmnor\\Projects\\LatexDemo\\yeet.png";
+            saveCanvas(fileName);
+            runScript();
         }
     }
 }
